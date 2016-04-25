c92cfcc98b20b68c63e5a0f3fdd853b99e0b81d0
commy2 <commy-2@gmx.de>
2016-04-25 11:39:06 +0200

    add CBA settings

---
+++ addons/settings/config.cpp
@@ -0,0 +1,21 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"CBA_common", "A3_UI_F", "3DEN"};
+        version = VERSION;
+        author[] = {"commy2"};
+        authorUrl = "https://github.com/CBATeam/CBA_A3";
+    };
+};
+
+#include "CfgEventHandlers.hpp"
+#include "CfgFunctions.hpp"
+
+#include "Cfg3DEN.hpp"
+#include "Display3DEN.hpp"
+
+#include "gui.hpp"
